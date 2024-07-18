79b8c71bb2585201a902601b9120bb22ef2effd0
030Affandika <100705931+030Affandika@users.noreply.github.com>
2024-07-18 09:52:21 +0700

    Update handler.js

--- src/handler.js
+++ src/handler.js
@@ -96,23 +96,24 @@ const getAllBookHandler = (request, h) => {
 };
 
 const getBookByIdHandler = (request, h) => {
-  const { bookId } = request.params;
-  const book = books.find((b) => b.id === bookId);
-
-  if (!book) {
-      return h.response({
-          status: 'fail',
-          message: 'Buku tidak ditemukan',
-      }).code(404);
-  }
-
-  return {
-      status: 'success',
-      data: {
-          book,
-      },
+    const { bookId } = request.params;
+    const book = books.find((b) => b.id === bookId);
+    if (!book) {
+        return h.response({
+            status: 'fail',
+            message: 'Buku tidak ditemukan',
+        }).code(404);
+    }
+    return h.response({
+        status: 'success',
+        data: {
+            book,
+        },
+    });
   };
-};
+
+
+
 
 
 
